485259b048fba8220f14f5d3a70ef3900692f458
AndrewKendig <90729156+AndrewKendig@users.noreply.github.com>
2023-08-04 11:22:28 -0400

    Basic window with PyQt6 and start of settings

--- main.py
+++ main.py
@@ -1,49 +1,118 @@
-from modules import window_manager, files_manager, settings, groups, versions
-import PySimpleGUI as sg
-import webbrowser
-import os
-import shutil
-# from PIL import Image
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QMenuBar, QFileDialog
+from PyQt6.QtGui import QAction
+import parts.data_handler as dh
 
-data_path = files_manager.init_data_folder(os)
 
-group_folder = ''
-version_folder = ''
-output_folder = ''
+version_number = '2.0'
 
-window_manager.change_theme(sg, 'DarkBlue17')  # 'DarkPurple7'
 
-window_object = window_manager.get_main_window_object(sg, os, data_path, files_manager)
-window = window_manager.make_window(sg, window_object)
+class MenuBar(QMenuBar):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.init_ui()
 
-# mainloop
-while True:
-    event, values = window.read(timeout=500)
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    elif event == 'Settings':
-        settings.show_settings_window(sg, window_manager)
-    elif event == 'Groups':
-        groups.show_groups_window(sg, os, window_manager, files_manager, data_path)
-        groups_list = files_manager.get_groups(os, data_path)
-        window.Element('-GROUP-').Update(values=groups_list)
-    elif event == 'Versions':
-        if os.path.exists(group_folder):
-            versions.show_versions_window(sg, os, window_manager, files_manager, group_folder)
-            versions_list = files_manager.get_versions(os, group_folder)
-            window.Element('-VERSION-').Update(values=versions_list)
-        else:
-            window_manager.show_not_implemented(sg, webbrowser)
-    elif event == 'Files':
-        window_manager.show_not_implemented(sg, webbrowser)
-    elif event == 'Select Group' and values['-GROUP-'] != '':
-        group_folder = data_path + values['-GROUP-'] + '\\'
-        output_folder = group_folder + 'Output\\'
-        versions_list = files_manager.get_versions(os, group_folder)
-        window.Element('-VERSION-').Update(values=versions_list)
-    elif event == 'Switch to Version' and values['-VERSION-'] != '':
-        version_folder = group_folder + 'Versions\\' + values['-VERSION-'] + '\\'
-    elif event == 'Apply Changes':
-        files_manager.push_files(shutil, version_folder, output_folder)
+    def init_ui(self):
+        file_menu = self.addMenu("File")
 
-window.close()
+        save_action = QAction("Save", self)
+        save_action.triggered.connect(self.parent().save_project)
+        file_menu.addAction(save_action)
+
+        open_action = QAction("Open", self)
+        open_action.triggered.connect(self.parent().open_project)
+        file_menu.addAction(open_action)
+
+        exit_action = QAction("Exit", self)
+        exit_action.triggered.connect(self.parent().close)
+        file_menu.addAction(exit_action)
+
+        edit_menu = self.addMenu("Edit")
+
+        display_folder_action = QAction("Set Display Folder", self)
+        display_folder_action.triggered.connect(self.parent().prompt_display_folder)
+        edit_menu.addAction(display_folder_action)
+
+        source_folder_action = QAction("Set Source Folder", self)
+        source_folder_action.triggered.connect(self.parent().prompt_source_folder)
+        edit_menu.addAction(source_folder_action)
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.data = None
+
+        self.init_ui()
+        self.init_data()
+
+    def init_ui(self):
+        self.setWindowTitle(f'Switch Witch v{version_number}')
+        self.setGeometry(100, 100, 800, 400)
+
+        # Create the central widget
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        # Create the main layout for the central widget
+        main_layout = QHBoxLayout(central_widget)
+        central_widget.setLayout(main_layout)
+
+        # Create the left column widget
+        left_column = QWidget(self)
+        main_layout.addWidget(left_column, 2)  # Weight 2 for 2:1 ratio
+
+        # Create the right column widget
+        right_column = QWidget(self)
+        main_layout.addWidget(right_column, 1)  # Weight 1 for 2:1 ratio
+
+        # Create the menu bar
+        menubar = MenuBar(self)
+        self.setMenuBar(menubar)
+
+    def init_data(self, display_folder=None, source_folder=None):
+        self.data = dh.Data(display_folder, source_folder)
+
+    def save_project(self):
+        if self.data:
+            if self.data.settings.settings_file:
+                self.data.settings.save_to_file()
+            else:
+                self.save_project_as()
+
+    def save_project_as(self):
+        if self.data:
+            file_path, _ = QFileDialog.getSaveFileName(self, "Save Switch Witch Project", "", "SWS Files (*.sws);;All Files (*)")
+            if file_path:
+                self.data.settings.save_to_file(file_path)
+
+    def open_project(self):
+        self.prompt_settings_file()
+        self.data.settings.load_from_file()
+
+    def prompt_display_folder(self):
+        if self.data:
+            folder_path = QFileDialog.getExistingDirectory(self, "Select Display Folder")
+            if folder_path:
+                self.data.settings.set_display_folder(folder_path)
+
+    def prompt_source_folder(self):
+        if self.data:
+            folder_path = QFileDialog.getExistingDirectory(self, "Select Source Folder")
+            if folder_path:
+                self.data.settings.set_source_folder(folder_path)
+
+    def prompt_settings_file(self):
+        if self.data:
+            file_path, _ = QFileDialog.getOpenFileName(self, "Open Switch Witch Project", "", "SWS Files (*.sws);;All Files (*)")
+            if file_path:
+                self.data.settings.set_settings_file(file_path)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ parts/data_handler.py
@@ -0,0 +1,69 @@
+import os
+import json
+
+
+class Data:
+    def __init__(self, display_folder=None, source_folder=None):
+        self.settings = Settings()
+
+        if display_folder:
+            self.settings.set_display_folder(display_folder)
+        if source_folder:
+            self.settings.set_source_folder(source_folder)
+
+
+class Settings:
+    def __init__(self):
+        self.display_folder = None
+        self.source_folder = None
+        self.settings_file = None
+
+    def save_settings(self):
+        settings_dict = dict(source_folder=self.source_folder,
+                             display_folder=self.display_folder)
+        settings_json = json.dumps(settings_dict)
+        return settings_json
+
+    def load_settings(self, settings_json):
+        settings_dict = json.loads(settings_json)
+        if 'display_folder' in settings_dict:
+            self.display_folder = settings_dict['display_folder']
+        if 'source_folder' in settings_dict:
+            self.source_folder = settings_dict['source_folder']
+
+    def save_to_file(self, settings_file=None):
+        if settings_file:
+            self.settings_file = settings_file
+        if self.settings_file:
+            settings_json = self.save_settings()
+            with open(self.settings_file, 'w') as file:
+                file.write(settings_json)
+            return True
+
+    def load_from_file(self, settings_file=None):
+        if settings_file:
+            if not self.set_settings_file(settings_file):
+                return False
+        if self.settings_file:
+            with open(self.settings_file, 'r') as file:
+                settings_json = file.read()
+                print(settings_json)
+                self.load_settings(settings_json)
+
+    def set_display_folder(self, display_folder):
+        if os.path.isdir(display_folder):
+            self.display_folder = display_folder
+            return True
+        return False
+
+    def set_source_folder(self, source_folder):
+        if os.path.isdir(source_folder):
+            self.source_folder = source_folder
+            return True
+        return False
+
+    def set_settings_file(self, settings_file):
+        if os.path.isfile(settings_file):
+            self.settings_file = settings_file
+            return True
+        return False
